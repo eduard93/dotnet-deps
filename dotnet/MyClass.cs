8a3c30b27250f659e9c9254e554aef556bf99f72
eduard93 <lebeduk93@gmail.com>
2020-07-15 10:44:44 +0300

    test case

---
+++ dotnet/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using CurrencyDotNetCore;
+using CurrencyDotNetCore.Model;
+
+namespace dc
+{
+    public class MyClass
+    {
+
+        public decimal GetRate()
+        {
+            CurrencyConverter currencyConverter = new CurrencyConverter();
+            decimal rate = currencyConverter.GetRate(Currency.EUR, Currency.USD);
+            return rate;
+        }
+    }
+}
